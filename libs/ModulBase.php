863225dc840e5c74262faea1ab8a237a26e86f5d
Burki24 <burkhard@kneiseler.de>
2024-10-28 12:05:05 +0100

    FIX: Destroy Message

--- libs/ModulBase.php
+++ libs/ModulBase.php
@@ -132,6 +132,7 @@ abstract class ModulBase extends \IPSModule
         $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
         $this->RegisterPropertyString('MQTTBaseTopic', '');
         $this->RegisterPropertyString('MQTTTopic', '');
+        $this->RegisterAttributeString('JsonFile', '');
         $this->TransactionData = [];
 
         // Vollständigen Pfad zum Verzeichnis für die Instanz-Dateien erstellen
@@ -198,9 +199,11 @@ abstract class ModulBase extends \IPSModule
         $files = glob($dateiPfad);
 
         foreach ($files as $file) {
+            $this->WriteAttributeString('JsonFile', $file);
+            $oldfile = ReadAttributeString('JsonFile');
             if (is_file($file)) {
                 if (unlink($file)) {
-                    $this->LogMessage(__CLASS__ . "Datei erfolgreich gelöscht: $file", KL_MESSAGE);
+                    $this->LogMessage(__CLASS__ . "Datei erfolgreich gelöscht: $oldfile", KL_MESSAGE);
                 } else {
                     $this->LogMessage(__CLASS__ . "Fehler beim Löschen der Datei: $file", KL_ERROR);
                 }
